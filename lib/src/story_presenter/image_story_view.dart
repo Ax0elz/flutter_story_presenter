abd3393e54a810930d387632f437ca2e065116e4
Ax0elz <alex.kotliarsky@gmail.com>
2025-03-05 16:36:56 -0700

    add key rto cached

--- lib/src/story_presenter/image_story_view.dart
+++ lib/src/story_presenter/image_story_view.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_story_presenter/flutter_story_presenter.dart';
@@ -145,6 +146,7 @@ class _ImageStoryViewState extends State<ImageStoryView> {
 
     /// If the image source is a network URL, use [CachedNetworkImage].
     return CachedNetworkImage(
+      key: ValueKey(widget.storyItem.url),
       imageUrl: widget.storyItem.url!,
       imageBuilder: (context, imageProvider) {
         // Mark the image as loaded once it is built.
